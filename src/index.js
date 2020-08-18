90c32f7c071e7c022b941241009425c9f5edf258
Christopher Kim <chrisms91@gmail.com>
2020-08-18 01:17:51 -0700

    practice...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+// function that modifies your data.
+// whatever this function returns, it becomes the state of the application.
+// this function can only modifies the data.
+// action must be an object and have type
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return (count += 1);
+    case MINUS:
+      return (count -= 1);
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
